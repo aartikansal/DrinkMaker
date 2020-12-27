640ee04f8c9a2df6b5aef290f376eb023e782fca
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-26 21:48:11 -0500

    Add files via upload

--- TeaMaker.java
+++ TeaMaker.java
@@ -1,13 +1,17 @@
 public class TeaMaker extends DrinkMaker {
+    @Override
     public void getRightCup() {
-
+        System.out.println("Grab a cup for tea");
     }
 
+    @Override
     public void addIngredients() {
-
+        System.out.println("Add tea leaves");
     }
 
+    @Override
     public void pour() {
-
+        System.out.println("Fill with boiling water");
     }
 }
+
